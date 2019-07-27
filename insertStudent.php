8eb90ab868a92113da85b2c87e5bf87691fce1b3
syedbilal2112 <syed.bilal.bs@gmail.com>
2019-07-27 16:24:30 +0530

    first commit

---
+++ insertStudent.php
@@ -0,0 +1,19 @@
+<?php 
+	include "conn.php";
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$address = $_POST['address'];
+	$branch = $_POST['branch'];
+	
+	$query = "INSERT INTO `students` (`student_name`,`student_email`,`student_address`, `student_branch`) VALUES('$name','$email','$address','$branch')";
+	$result = mysqli_query($con,$query);
+
+	if($result){
+		echo "successfully inserted";
+	}
+	else{
+		echo "failed to insert";
+	}
+
+ ?>
